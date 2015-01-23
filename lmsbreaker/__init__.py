7bb4bad11a1853855ad8952187c6c8f523e3bc93
soin08 <ilya@soin.rocks>
2015-01-23 23:12:23 +0300

    LMS_BaseError import fix

--- lmsbreaker/__init__.py
+++ lmsbreaker/__init__.py
@@ -4,3 +4,4 @@ from .breaker import LMS_SessionError
 from .breaker import LMS_UnknownError
 from .breaker import LMS_UnitError
 from .breaker import LMS_PercentError
+from .breaker import LMS_BaseError
